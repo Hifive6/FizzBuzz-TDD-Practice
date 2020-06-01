608dc296973d6ff1c389649bcc3787e93aca862b
David Juarez <djuar05@gmail.com>
2020-06-01 10:15:14 -0400

    new methods for fizz and buzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,10 +6,25 @@ public class FizzBuzz {
     }
 
     public String processNumber(int number){
-        if(number == 3){
+        if(isMulitpleOf3(number) && isMulitpleOf5(number)){
+            return "Fizz Buzz";
+        }
+        else if(isMulitpleOf3(number)){ //if number is divisible by 3, remaninder will be 0
             return "Fizz";
+        } else if(isMulitpleOf5(number)) {//if number is divisible by 5, remaninder will be 0
+            return "Buzz";
         } else {
-        return Integer.toString(number);
+            return Integer.toString(number);
         }
     }
+
+    private boolean isMulitpleOf3(int number){
+        return number % 3 == 0;
+    }
+    private boolean isMulitpleOf5(int number){
+        return number % 5 == 0;
+    }
+    
+
+    
 }
